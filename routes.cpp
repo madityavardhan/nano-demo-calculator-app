ffc882b0da2635e063c61eec6f4e7290ff5ff02a
madityavardhan <madityavardhan03@gmail.com>
2023-08-16 01:04:01 +0530

    test2

--- routes.cpp
+++ routes.cpp
@@ -7,12 +7,14 @@ crow::response greet()
 crow::response add(const crow::request &req)
 {
     auto input = crow::json::load(req.body);
-
-    return crow::response{"result":};
+    int a = input["first"].i();
+    int b = input["second"].i();
+    return crow::response{"result":(a+b)};
 }
 crow::response subtract(const crow::request &req)
 {
     auto input = crow::json::load(req.body);
-    
-    return crow::response{"result":};
+    int a = input["first"].i();
+    int b = input["second"].i();
+    return crow::response{"result":(a-b)};
 }
